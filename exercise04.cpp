1f8dc45358513353819c54e952f320979236b4f5
Maneesha Palihawadana <100304099+IT21264948@users.noreply.github.com>
2022-03-03 18:07:29 +0000

    done

--- exercise04.cpp
+++ exercise04.cpp
@@ -2,20 +2,29 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int& length, int& width);
 
-// Do not change the main() function
+
 int main() {
    int length = 10, width = 5;
    input(length, width);
    print(length, width);
    return 0;
 }
-
-// Do not change the print() function
-void print(int len, int wth) {
+void print(int len, int wth) 
+{
    cout << "Length : " << len 
         << ", Width  : " << wth << endl;
 }
+void input(int& length,int& width)
+{
+   cout<<"Enter the length:";
+   cin>>length;
+   cout<<"Enter the width:";
+   cin>>width;
+   
 
-// Implement the Input Function here
+  
+}
+
+
